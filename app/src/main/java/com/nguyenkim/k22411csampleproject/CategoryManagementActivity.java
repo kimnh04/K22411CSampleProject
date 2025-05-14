e372357bd984da7b1f14cbc86e7016695f85537e
Nguyễn Kim <nguyenkim@MacBook-Air-cua-Nguyen-52.local>
2025-05-14 19:21:44 +0700

    Đã hiện được list

--- app/src/main/java/com/nguyenkim/k22411csampleproject/CategoryManagementActivity.java
+++ app/src/main/java/com/nguyenkim/k22411csampleproject/CategoryManagementActivity.java
@@ -1,6 +1,10 @@
 package com.nguyenkim.k22411csampleproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.nguyenkim.connectors.CategoryConnector;
+import com.nguyenkim.connectors.CustomerConnector;
+import com.nguyenkim.k22411csampleproject.models.Category;
+import com.nguyenkim.k22411csampleproject.models.Customer;
+
 public class CategoryManagementActivity extends AppCompatActivity {
+    ListView lvCategory;
+    ArrayAdapter<Category> adapter;
+    CategoryConnector connector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,30 @@ public class CategoryManagementActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addViews();
+        addEvents();
     }
+
+    private void addEvents() {
+        lvCategory.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+                Category selected = (Category) adapter.getItem(i);
+                adapter.remove(selected);
+                return true;
+            }
+        });
+    }
+
+    private void addViews() {
+
+        lvCategory = findViewById(R.id.lvCategory);
+        adapter=new ArrayAdapter<>(
+                CategoryManagementActivity.this, android.R.layout.simple_list_item_1
+        );
+        connector=new CategoryConnector();
+        adapter.addAll(connector.getAllCategories());
+        lvCategory.setAdapter(adapter);
+    }
+
 }
--- app/src/main/java/com/nguyenkim/k22411csampleproject/ProductManagementActivity.java
+++ app/src/main/java/com/nguyenkim/k22411csampleproject/ProductManagementActivity.java
@@ -1,6 +1,10 @@
 package com.nguyenkim.k22411csampleproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.nguyenkim.connectors.CategoryConnector;
+import com.nguyenkim.connectors.ProductConnector;
+import com.nguyenkim.k22411csampleproject.models.Category;
+import com.nguyenkim.k22411csampleproject.models.Product;
+
 public class ProductManagementActivity extends AppCompatActivity {
+    ListView lvProduct;
+    ArrayAdapter<Product> adapter;
+    ProductConnector connector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,28 @@ public class ProductManagementActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addViews();
+        addEvents();
+    }
+
+
+    private void addEvents() {
+        lvProduct.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+                Product selected = (Product) adapter.getItem(i);
+                adapter.remove(selected);
+                return true;
+            }
+        });
+    }
+    private void addViews() {
+        lvProduct = findViewById(R.id.lvProduct);
+        adapter=new ArrayAdapter<>(
+                ProductManagementActivity.this, android.R.layout.simple_list_item_1
+        );
+        connector=new ProductConnector();
+        adapter.addAll(connector.getAllProducts());
+        lvProduct.setAdapter(adapter);
     }
 }
